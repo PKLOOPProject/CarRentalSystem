77e1d2804c0816ed4a73276fe22503427d9bd265
PKLOOPProject <90744436+PKLOOPProject@users.noreply.github.com>
2021-10-13 18:11:02 +1030

    Add files via upload

---
+++ Bookings.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+#include<stdlib.h>
+using namespace std;
+#include"Vehicle.h"
+#include"Car.h"
+#include"Truck.h"
+#include"Bookings.h"
+
+string Bookings::id_maker(int info_booking, Bookings list[])
+{
+    cout<<"Your booking details are: "<<endl;
+    cout<<"\t\t\tName: "<<list[info_booking].get_name()<<endl; 
+    cout<<"\t\t\tBooking Start Date: "<<list[info_booking].get_sdate()<<endl;
+    cout<<"\t\t\tBooking End Date: "<<list[info_booking].get_edate()<<endl;
+    cout<<"\t\t\tBooking Status: "<<list[info_booking].get_status()<<endl;
+    cout<<"\t\t\tBooking Remainder: "<<list[info_booking].get_remainder()<<endl;
+}
