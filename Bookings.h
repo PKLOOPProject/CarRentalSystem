e9df3b8eb94cf7ca629665281c058991f70c7c26
PKLOOPProject <90744436+PKLOOPProject@users.noreply.github.com>
2021-10-13 23:08:17 +1030

    Update Bookings.h

--- Bookings.h
+++ Bookings.h
@@ -8,16 +8,31 @@ using namespace std;
 class Bookings
 {
     public:
-    string id_maker();
+    string id_maker(int info_booking, Bookings list[]);
+    void set_id();
+    void set_name();
+    void set_sdate();
+    void set_edate();
+    void set_status();
+    void set_remainder();
+    void set_chosencar();
+
+    string get_id();
+    string get_name();
+    string get_sdate();
+    string get_edate();
+    bool get_status();
+    int get_remainder();
+    Car get_chosencar();
+
+    private:
     string Booking_id;
     string Booking_name;
     string Booking_date;
-    string Start_date;
     string End_date;
     bool Payment_status; 
     int remainder;
     Car assigned_car;
-
 
 };
 #endif
